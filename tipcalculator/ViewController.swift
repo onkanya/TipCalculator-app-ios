b22300c3a42a323799d74d39a40cc9f469c8b598
Onkanya <onkanyalearning@gmail.com>
2019-11-15 16:39:05 +0700

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Mac on 15/11/19.
+//  Copyright © 2019 mean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
